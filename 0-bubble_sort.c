b2cb34a9464bcadb583fe7405d169b3a4dac53f2
Bwegesa <wegesamaroa.7@gmail.com>
2023-07-25 16:34:48 -0700

    0-bubble_sort.c Write a function that sorts an array of integers in ascending order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,41 +1,33 @@
 #include "sort.h"
-#include <stddef.h>
-#include <stdio.h>
-
 /**
- * bubble_sort - sorts an arr in ascending order using bubble sort
- * @array: array to be sorted
- * @size: size of the array to be sorted
+ *bubble_sort - Function that sorts an array of ints
+ *using bubble sort algorithm
+ *
+ *@array: array of ints
+ *@size: size of array
+ *
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t w, y;
 	int temp;
+	size_t i, k;
 
-	/**
-	 * The outer for loop iterates over the array/ its the passes
-	 * w < size - 1, ensures the loop will iterate over the array
-	 * until w reaches the 2nd last index. It ensures that during
-	 * each iteration for outer loop, the algorithm can compare
-	 * each element with its adjacent element and perform the
-	 * neccessary swap if the order is incorrect
-	 * The inner loop iterates only over unsorted array hence the
-	 * y < size - 1 - w
-	 */
-	if (size < 2)
+	if (!array || !size)
 		return;
 
-	for (w = 0; w < size - 1; w++)
+	i = 0;
+	while (i < size)
 	{
-		for (y = 0; y < size - 1 - w; y++)
+		for (k = 0; k < size - 1; k++)
 		{
-			if (array[y] > array[y + 1])
+			if (array[k] > array[k + 1])
 			{
-				temp = array[y];
-				array[y] = array[y + 1];
-				array[y + 1] = temp;
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
 				print_array(array, size);
 			}
 		}
+		i++;
 	}
 }
